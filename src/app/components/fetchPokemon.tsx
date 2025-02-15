c11cdb3e816469a50106ad1d4718154bd8635700
Megan Walford <mwalford22@gmail.com>
2025-02-15 22:42:00 +0000

    Connected FetchAPI to pull information about Pikachu

---
+++ src/app/components/fetchPokemon.tsx
@@ -0,0 +1,37 @@
+export default async function Pokemon() {
+  const getPokemon = async () => {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu/");
+    return response.json();
+  };
+
+  const data = await getPokemon();
+  console.log(data);
+
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          id="search-pokemon"
+          className="border border-black rounded-md m-5 w-96  p-1"
+          placeholder="Search by Pokemon name..."
+        />
+        <button className="bg-indigo-500 py-1 px-3 rounded-md">Search!</button>
+      </div>
+
+      {/* map through list */}
+      <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-8">
+        <div className="bg-white shadow-md rounded-lg border border-indigo-800 m-3 p-6 w-48 text-center">
+          <img
+            src={data.sprites.front_default}
+            alt="Pokemon Image"
+            id="spriteImage"
+            className="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"
+          />
+          <h2 className="text-lg font-semibold text-gray-800">{data.name}</h2>
+          <p className="text-sm text-gray-500">{data.weight}</p>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,75 +1,15 @@
-import Image from "next/image";
+import FetchPokemon from "@/app/components/fetchPokemon";
 
 export default function Home() {
   return (
     <div>
       <header>
         <div className="bg-indigo-500 p-7 font-mono">
-          <h1 className="text-4xl font-bold text-white">Pokemon Fetch</h1>
+          <h1 className="text-4xl font-bold text-white">Pokémon Fetch</h1>
         </div>
       </header>
       <main>
-        <div>
-          <input
-            type="text"
-            id="search-pokemon"
-            className="border border-black rounded-md m-5 w-96  p-1"
-            placeholder="Search by Pokemon name..."
-          />
-          <button className="bg-indigo-500 py-1 px-3 rounded-md">
-            Search!
-          </button>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-8">
-          <div className="bg-white shadow-md rounded-lg border border-indigo-800 m-3 p-6 w-48 text-center">
-            <img
-              src=""
-              alt="Pokemon Image"
-              id="spriteImage"
-              className="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"
-            />
-            <h2 id="spriteName" className="text-lg font-semibold text-gray-800">
-              Pokemon
-            </h2>
-            <p className="text-sm text-gray-500">Additional</p>
-          </div>
-          <div className="bg-white shadow-md rounded-lg border border-indigo-800 m-3 p-6 w-48 text-center">
-            <img
-              src=""
-              alt="Pokemon Image"
-              id="spriteImage"
-              className="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"
-            />
-            <h2 id="spriteName" className="text-lg font-semibold text-gray-800">
-              Pokemon
-            </h2>
-            <p className="text-sm text-gray-500">Additional</p>
-          </div>
-          <div className="bg-white shadow-md rounded-lg border border-indigo-800 m-3 p-6 w-48 text-center">
-            <img
-              src=""
-              alt="Pokemon Image"
-              id="spriteImage"
-              className="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"
-            />
-            <h2 id="spriteName" className="text-lg font-semibold text-gray-800">
-              Pokemon
-            </h2>
-            <p className="text-sm text-gray-500">Additional</p>
-          </div>
-          <div className="bg-white shadow-md rounded-lg border border-indigo-800 m-3 p-6 w-48 text-center">
-            <img
-              src=""
-              alt="Pokemon Image"
-              id="spriteImage"
-              className="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-4"
-            />
-            <h2 id="spriteName" className="text-lg font-semibold text-gray-800">
-              Pokemon
-            </h2>
-            <p className="text-sm text-gray-500">Additional</p>
-          </div>
-        </div>
+        <FetchPokemon />
       </main>
     </div>
   );
